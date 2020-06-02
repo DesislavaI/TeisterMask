7269d66df2e21eb3d5f9d16106da120824f6d748
DesislavaI <d_iswriting@yahoo.com>
2020-06-02 18:50:38 +0300

    Technology Fundamentals - My first web app

---
+++ TeisterMask/Migrations/20200602125509_Inintial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TeisterMask.Migrations
+{
+    public partial class Inintial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaskStatus",
+                table: "Tasks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Tasks",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Tasks");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TaskStatus",
+                table: "Tasks",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
